1f6344c0cdb14d40b3ca06f8e256cabe75c61899
Artem Cherednychenko <artem.cherednychenko2817@gmail.com>
2024-11-18 17:30:17 +0100

    First

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("My name Artem!");
+int a = 5;
+int b = 6;
+Console.WriteLine(a + b);
